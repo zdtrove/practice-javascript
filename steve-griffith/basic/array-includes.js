06b46915051cf3ab9aab4abb670eaabc04f6ceb6
Your Name <you@example.com>
2020-08-09 22:43:16 +0700

    cm

---
+++ steve-griffith/basic/array-includes.js
@@ -0,0 +1,17 @@
+// Array includes() method
+// check to see if something is inside an array
+// method returns a Boolean answer
+
+let dwarves = ['Grumpy', 'Sneezy', 'Happy', 'Bashful', 'Doc', 'Dopey', 'Sleepy']
+
+let name1 = 'Peter Dinklage'
+let name2 = 'Kenny Baker'
+let name3 = 'Happy'
+
+let hasPeter = dwarves.includes(name1)
+let hasKenny = dwarves.includes(name2)
+let hasHappy = dwarves.includes(name3)
+
+console.log('contains Peter', hasPeter)
+console.log('contains Kenny', hasKenny)
+console.log('contains Hanny', hasHappy)
